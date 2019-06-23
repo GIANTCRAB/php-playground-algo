0165cf7b38fe26b1b74fbae202de8e7bfbcba1df
Huiren Woo <giantcrabby@gmail.com>
2019-06-23 22:24:49 +0800

    Reduce method further to truly O(n)

--- minesweeper.php
+++ minesweeper.php
@@ -11,32 +11,37 @@ class MineSweeper
     {
         $this->columns = $columns;
         $this->mines = $mines;
-        $this->totalCount = $this->columns * $rows;
+        $this->totalCount = ($this->columns + 1) * $rows - 1;
     }
 
     public function parseMines()
     {
-        $minesWithoutNewlines = str_replace(["\n\r", "\n", "\r"], '', $this->mines);
-        $minesWithZero = str_replace('.', 0, $minesWithoutNewlines);
-        $minesAsChars = str_split($minesWithZero);
+        $minesAsChars = str_split($this->mines);
 
         for ($i = 0; $i < $this->totalCount; $i++) {
             if ($minesAsChars[$i] === '*') {
-                $iPlusOneModuloColumns = ($i + 1) % $this->columns;
+                $columnsPlusOne = $this->columns + 1;
+                $iPlusOneModuloColumns = ($i + 1) % $columnsPlusOne;
 
                 if ($i === 0 || $iPlusOneModuloColumns === 1) {
-                    $positions = [$i - 5, $i - 4, $i + 1, $i + 5, $i + 6];
-                } elseif($i === $this->totalCount - 1 || $iPlusOneModuloColumns === 0) {
-                    $positions = [$i - 6, $i - 5, $i - 1, $i + 4, $i + 5];
+                    $positions = [$i - $this->columns - 1, $i - $this->columns, $i + 1, $i + $this->columns + 1, $i + $this->columns + 2];
+                } elseif ($i === $this->columns || $iPlusOneModuloColumns === $this->columns) {
+                    $positions = [$i - $this->columns - 2, $i - $this->columns - 1, $i - 1, $i + $this->columns, $i + $this->columns + 1];
                 } else {
-                    $positions = [$i - 6, $i - 5, $i - 4, $i -1, $i + 1, $i + 4, $i + 5, $i + 6];
+                    $positions = [$i - $this->columns - 2, $i - $this->columns - 1, $i - $this->columns, $i - 1, $i + 1, $i + $this->columns, $i + $this->columns + 1, $i + $this->columns + 2];
                 }
 
                 foreach ($positions as $position) {
                     if ($position >= 0 && $position < $this->totalCount && $minesAsChars[$position] !== '*') {
-                        $minesAsChars[$position] = (int) $minesAsChars[$position] + 1;
+                        if ($minesAsChars[$position] === '.') {
+                            $minesAsChars[$position] = 1;
+                        } else {
+                            ++$minesAsChars[$position];
+                        }
                     }
                 }
+            } elseif ($minesAsChars[$i] === '.') {
+                $minesAsChars[$i] = 0;
             }
         }
 
@@ -45,14 +50,11 @@ class MineSweeper
         return $this;
     }
 
-    public function getMineMap()
+    public function getMineMap(): string
     {
         $result = '';
 
-        for($i = 0; $i < $this->totalCount; $i++) {
-            if($i !== 0 && $i % $this->columns === 0) {
-                $result .= "\n";
-            }
+        for ($i = 0; $i < $this->totalCount; $i++) {
             $result .= $this->map[$i];
         }
 
@@ -73,7 +75,7 @@ $expectedResult = "*1000
 *1000
 11000";
 
-if($mineMap === $expectedResult) {
+if ($mineMap === $expectedResult) {
     echo 'test 1 passed';
 }
 
@@ -92,7 +94,7 @@ $expectedResult = "0001*
 0001*
 00011";
 
-if($mineMap === $expectedResult) {
+if ($mineMap === $expectedResult) {
     echo 'test 2 passed';
 }
 
@@ -112,6 +114,50 @@ $expectedResult = "*1000
 1*100
 11100";
 
-if($mineMap === $expectedResult) {
+if ($mineMap === $expectedResult) {
     echo 'test 3 passed';
+}
+
+echo "\n\n";
+
+$mines = "*.....
+......
+.*....
+......
+.....*";
+
+$mineSweeper = new MineSweeper(6, 5, $mines);
+$mineMap = $mineSweeper->parseMines()->getMineMap();
+
+$expectedResult = "*10000
+221000
+1*1000
+111011
+00001*";
+
+if ($mineMap === $expectedResult) {
+    echo 'test 4 passed';
+}
+
+echo "\n\n";
+
+$mines = "*......
+.......
+.*.....
+.......
+.....*.
+.......";
+
+$mineSweeper = new MineSweeper(7, 6, $mines);
+$mineMap = $mineSweeper->parseMines()->getMineMap();
+
+$expectedResult = "*100000
+2210000
+1*10000
+1110111
+00001*1
+0000111";
+
+if ($mineMap === $expectedResult) {
+    echo 'test 5 passed';
 }
